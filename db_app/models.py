3d3f32de4df45cbe7c1772b3aed0958007d2335d
Houdaaaa <Houda-11.03@hotmail.com>
2019-10-09 23:07:02 +0200

    updating classes and function to db_access changing driver to py2neo_v4

--- db_app/models.py
+++ db_app/models.py
@@ -1,7 +1,11 @@
-from neo4j import GraphDatabase, basic_auth
+from py2neo import Graph, Node, Relationship, NodeMatcher, matching
 
 uri = "bolt://localhost:7687"
-driver = GraphDatabase.driver(uri, auth=("neo4j", "editx"))
+#graph = Graph(username='neo4j', password='editx')
+
+graph = Graph(uri, user="neo4j", password="editx")
+matcher = NodeMatcher(graph)
+
 
 class Field():
     def __init__(self, name, level):
@@ -29,139 +33,143 @@ class Database():   #classe statique?
     def __init__(self, password):
         self.password = password
 
-    def add_field(self, field):
+    @staticmethod
+    def add_field(field):
         name = field.get_name()
         level = field.get_level()
+        fieldNode = Node('Field', name=name, level=level) #id?
+        graph.create(fieldNode)
 
-        with driver.session() as session:
-            cql_query = """CREATE (f:Field {name: '%s', level: '%d'})""" %(name, level)
-            session.run(cql_query)
-
-    def add_question(self, question):
+    @staticmethod
+    def add_question(question):
         title = question.get_title()
         answer = question.get_answer()
+        questionNode = Node('Question', title=title, answer=answer)
+        graph.create(questionNode)
 
-        with driver.session() as session:
-            cql_query = """CREATE (q:Question {title: '%s', answer: '%s'})""" %(title, answer)
-            session.run(cql_query)
-
-    def add_subfield_relationship(self, field, subfield):
+    @staticmethod
+    def add_subfield_relationship(field, subfield):
         fieldLevel = field.get_level()
         fieldName = field.get_name()
         subfieldLevel = subfield.get_level()
         subfieldName = subfield.get_name()
 
         if (fieldLevel < subfieldLevel):
-            with driver.session() as session:
-                cql_query = """MATCH (f1: Field{name: '%s', level: '%d'})
-                               MATCH (f2: Field{name: '%s', level: '%d'})
-                               CREATE (f1)-[rel: subfield]->(f2)""" %(fieldName, fieldLevel, subfieldName, subfieldLevel)
-                session.run(cql_query)
+            f1 = matcher.match("Field", name=fieldName).first()
+            f2 = matcher.match("Field", name=subfieldName).first()
+            graph.merge(Relationship(f1, 'subfield', f2))
         else:
             print("the level of the first param must be smaller than the level of the second")
 
-    def add_is_linked_to_relationship(self, field1, field2):
+    @staticmethod
+    def add_is_linked_to_relationship(field1, field2):
         field1Level = field1.get_level()
         field1Name = field1.get_name()
         field2Level = field2.get_level()
         field2Name = field2.get_name()
 
         if(field1Level > field2Level):
-            with driver.session() as session:
-                cql_query = """MATCH (f1: Field{name: '%s', level: '%d'})
-                               MATCH (f2: Field{name: '%s', level: '%d'})
-                               CREATE (f1)-[rel: is_linked_to]->(f2)""" %(field1Name, field1Level, field2Name, field2Level)
-                session.run(cql_query)
-
+            f1 = matcher.match("Field", name=field1Name, level=field1Level).first()
+            f2 = matcher.match("Field", name=field2Name, level=field2Level).first()
+            graph.merge(Relationship(f1, 'is_linked_to', f2))
         else:
             print("the level of the first param must be greater than the level of the second")
 
-    def add_question_relationship(self, subfield, question):
+    @staticmethod
+    def add_question_relationship(subfield, question):
         subfieldName = subfield.get_name()
         subfieldLevel = subfield.get_level()
         questionTitle = question.get_title()
 
-        with driver.session() as session:
-            cql_query = """MATCH (f: Field{name: '%s', level: '%d'})
-                           MATCH (q: Question{title: '%s'})
-                           CREATE (f)-[rel: question]->(q)""" % (subfieldName, subfieldLevel, questionTitle)
-            session.run(cql_query)
+        f = matcher.match("Field", name=subfieldName, level=subfieldLevel).first()
+        q = matcher.match("Question", title=questionTitle).first()
+        graph.merge(Relationship(f, 'subfield', q))
 
-    def find_one_field(self, name): #or param = object field?
-        with driver.session() as session:
-            cql_query = """MATCH (f: Field{name: '%s'}) RETURN f """ % (name)
-            return session.run(cql_query)
+    '''return : object Node'''
+    @staticmethod
+    def find_one_field(name): #or param = object field?
+            fieldNode = matcher.match("Field", name=name).first()
+            return fieldNode
 
-    def find_subfields(self, field):
+    '''for questions and subfiels of a field'''
+    @staticmethod
+    def find_nodes(field, relationName):
+        fieldLevel = field.get_level()
+        fieldName = field.get_name()
+        f = Database.find_one_field(fieldName)
+        nodeList=[]
+        for rel in graph.match((f,), r_type=relationName):
+            nodeList.append(rel.end_node)   #type of rel.end_node = Node()
+        #print(nodeList[0]['name']) #ok
+        return nodeList
+
+    @staticmethod
+    def find_same_level_fields(level):
+        fieldsNodes = matcher.match("Field", level=level)
+        nodesList=[]
+        for field in fieldsNodes:
+            nodesList.append(field)
+        #print(nodeList[2]['name'])
+        return nodesList
+
+    '''faut-t-il afficher les questions même pour un niveau 1?'''
+    @staticmethod
+    def find_questions(field):
         fieldLevel = field.get_level()
         fieldName = field.get_name()
 
-        with driver.session() as session:
-            cql_query = """MATCH (f: Field{name: '%s', level: '%d'})-[rel: subfield*]->(f2: Field)
-                           RETURN f2.name, f2.level""" % (fieldName, fieldLevel)
-            return session.run(cql_query)
-
-    def find_same_level_fields(self, level):
-        with driver.session() as session:
-            cql_query = """MATCH (f: Field{level: '%d'})
-                           RETURN f.name""" % (level)
-            return session.run(cql_query)
-
-    def find_questions(self, field):
+    '''delete the field and all its relationships'''
+    @staticmethod
+    def delete_field(field):        #supprime tous les noeuds ayant le même nom, utiliser id? corriger pour avoir field avec nom unique
         fieldLevel = field.get_level()
         fieldName = field.get_name()
-        with driver.session() as session:
-            cql_query = """MATCH (f: Field{name: '%s', level: '%d'})-[rel: question]->(q: Question)
-                           RETURN q.title""" % (fieldName, fieldLevel)
-            return session.run(cql_query)
+        fieldNode = Database.find_one_field(fieldName)
+        graph.delete(fieldNode)
 
-    def delete_field(self, field):
-        fieldLevel = field.get_level()
-        fieldName = field.get_name()
-
-        with driver.session() as session:
-            cql_query = """MATCH (f: Field{name: '%s', level: '%d'}) DETACH DELETE (f)""" % (fieldName, fieldLevel)
-            return session.run(cql_query)
-
-    def delete_question(self, question):
+    @staticmethod
+    def delete_question(question):
         title = question.get_title()
+        questionNode = matcher.match("Question", title=title).first()
+        graph.delete(questionNode)
 
-        with driver.session() as session:
-            cql_query = """MATCH (q: Question{title: '%s'}) DETACH DELETE (q)""" % (title)
-            return session.run(cql_query)
-
-    def delete_relation(self, field1, field2, relationName):
+    @staticmethod
+    def delete_relation(field1, field2, relationName):
         field1Name = field1.get_name()
         field1Level = field1.get_level()
         field2Name = field2.get_name()
         field2Level = field2.get_level()
 
-        with driver.session() as session:
-            cql_query = """MATCH (f1: Field{name: '%s', level: '%d'})-[rel:%s]->(f2: Field{name: '%s', level: '%d'})
-                           DELETE (rel)""" % (field1Name, field1Level, relationName, field2Name, field2Level)
-            return session.run(cql_query)
+        startNode= Database.find_one_field(field1Name)  #add levels?
+        endNode= Database.find_one_field(field2Name)
 
-    def modify_field(self, field, newField): #modify the properties only
+        relationship = graph.match_one(nodes= (startNode, endNode), r_type=relationName)
+        graph.separate(relationship)
+
+    @staticmethod
+    def modify_field(field, newField): #modify the properties only
         fieldName = field.get_name()
         fieldLevel = field.get_level()
         newFieldName = newField.get_name()
         newFieldLevel = newField.get_level()
 
-        with driver.session() as session:
-            cql_query = """MATCH (f: Field{name: '%s', level: '%d'})
-                           SET f.name = '%s', f.level = '%d'""" % (fieldName, fieldLevel, newFieldName, newFieldLevel)
-            return session.run(cql_query)
+        fieldNode=Database.find_one_field(fieldName)
+        alle=Node('Field', id=fieldNode['id'])
+        newFieldNode=Node('Field', name=newFieldName, level=newFieldLevel)
+        graph.merge(alle, 'Field', fieldNode['id'])
+        alle['level']=3
+        alle.push()
 
-    def modify_question(self, question, newQuestion):
+
+
+    @staticmethod
+    def modify_question(question, newQuestion):
         title = question.get_title()
         answer = question.get_answer()
         newTitle = newQuestion.get_title()
         newAnswer = newQuestion.get_answer()
-
-        with driver.session() as session:
-            cql_query = """MATCH (q: Question{title: '%s', answer: '%s'})
-                           SET q.title = '%s', q.answer = '%s' """ % (title, answer, newTitle, newAnswer)
-            return session.run(cql_query)
+        cql_query = """MATCH (q: Question{title: '%s', answer: '%s'})
+                       SET q.title = '%s', q.answer = '%s' """ % (title, answer, newTitle, newAnswer)
+        return graph.run(cql_query)
 
     #def delete_subgraph
     #def add subgraph
--- db_app/views.py
+++ db_app/views.py
@@ -1,8 +1,23 @@
-#from .models import
+from .models import Field, Question, Database
 from flask import Flask, request, session, redirect, url_for, render_template, flash
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    f= Field("os", 1)
+    f2= Field("file managment",2)
+    f3= Field("management", 1)
+    f4=Field("os222", 2)
+    q= Question("How are you?", "Houda")
+    f5= Field("computer systems", 3)
+    #oui = Database.find_one_field('os')
+    #Database.add_field(f2)
+    #Database.add_subfield_relationship(f3, f4)
+    #Database.add_subfield_relationship(f3,f2)
+    os = Field('operating systems', 2)
+    Database.modify_field(f2, f5)
+    #Database.delete_field(os)
+    #Database.delete_relation(f3, f4, "subfield")
+    #next(oui)['f']['name']
+    return render_template('index.html', newfield = "coucou")
