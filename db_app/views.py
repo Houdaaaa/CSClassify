a3b995d6a516ef26387dda9c8ddb45ab33054d90
Houdaaaa <Houda-11.03@hotmail.com>
2019-10-14 16:11:33 +0200

    little code cleaning

--- db_app/views.py
+++ db_app/views.py
@@ -5,25 +5,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # if Database.find_sub_nodes(field, 'subfield') != None
-    rootFieldsList = Database.find_same_level_fields(1)
-    fieldsL2List = {}
-
-    f1 = Field('operating systems',1)
-    f2 = Field('computer systems', 1)
-    f3 = Field('programming', 1)
-    f4 = Field('storage management', 2)
-    f5 = Field('files systems', 3)
-    f6 = Field('memory management', 3)
-    f7 = Field('os types', 2)
-    f8 = Field('languages', 2)
-    f9 = Field('paradigms', 2)
-    f10 = Field('imperative', 3)
-
-
-    levels_1_2, levels_2_3 = Database.find_subfields_2(f1)
-    allFields = Database.find_subfields_3()
-
-
-
-    return render_template('index.html', allFields=allFields )
+    allFields = Database.find_all_fields()
+    return render_template('index.html', allFields=allFields)
